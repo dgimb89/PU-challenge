25ab8c302a6d927ea8025549fa98666c33e031d9
Daniel Gimbatschki <daniel@gimbatschki.de>
2022-11-12 11:50:31 +0100

    Make SOURCES_FETCH_TIMEOUT_MS configurable via .env

--- src/flights/flights.module.ts
+++ src/flights/flights.module.ts
@@ -8,6 +8,7 @@ import { FlightSourceConsumer } from './flight-source.consumer';
 import { FlightsService } from './flights.service';
 
 const DEFAULT_JOB_LIMIT_PER_SECOND = 10;
+const DEFAULT_SOURCES_FETCH_TIMEOUT_MS = 5000; // 5 seconds
 
 @Module({
   imports: [
@@ -26,10 +27,14 @@ const DEFAULT_JOB_LIMIT_PER_SECOND = 10;
         },
       }),
     }),
-    // TODO: Make this configurable in environment variables.
-    // https://docs.nestjs.com/techniques/http-module#async-configuration
-    HttpModule.register({
-      timeout: 5000,
+    HttpModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        timeout:
+          configService.get('SOURCES_FETCH_TIMEOUT_MS') ||
+          DEFAULT_SOURCES_FETCH_TIMEOUT_MS,
+      }),
     }),
   ],
   controllers: [FlightsController],
